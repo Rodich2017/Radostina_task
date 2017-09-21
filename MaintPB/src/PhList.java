2bfd696cbd1e063a01220aed723d3929b2053afa
Rodich2017 <31790104+Rodich2017@users.noreply.github.com>
2017-09-21 22:03:50 +0300

    Add files via upload

---
+++ MaintPB/src/PhList.java
@@ -0,0 +1,18 @@
+
+public class PhList {
+
+	public static void main(String[] args) {
+
+		Owner owner = new Owner();
+		owner.setFullName("Victor Peshev");
+		owner.setMailAddress("peshev@some.com");
+		owner.setDeptName("MM");
+		owner.setPosDescr("Category Manager");
+		owner.setPhNumber("0888230230");
+		
+		System.out.println(owner.toString());
+		
+
+	}
+
+}
---
+++ MaintPB/src/Provider.java
@@ -0,0 +1,9 @@
+
+public class Provider {
+
+	String username = "tstuser";
+	String password = "well";
+	String url = "jdbc:postgresql://localhost:5432/Radostina_task";	
+
+
+}
